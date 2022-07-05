48ff7f02a059486eee921964149dd976407d4e66
robinshrestha12 <robinshrestha.maple@gmail.com>
2022-07-05 13:01:13 -0400

    working order page first edit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,9 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    #line_items = @order.line_items
+   # puts "+++++++++++++++"
+    #puts line_items.count
   end
 
   def create
