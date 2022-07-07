b21e5730e58b3fbd01f99917e863621996ae4596
robinshrestha12 <robinshrestha.maple@gmail.com>
2022-07-07 14:13:51 -0400

    user login prepare

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV["USERNAME"],
+  password: ENV["PASSWORD"],
+  if: -> { ENV["PASSWORD"].present? }
+
+protect_from_forgery with: :exception
+  
   def show
     @product_count = Product.count
     @categories_count = Category.count
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # user authentication
-  http_basic_authenticate_with name: ENV["USERNAME"],
-                               password: ENV["PASSWORD"],
-                               if: -> { ENV["PASSWORD"].present? }
 
-  protect_from_forgery with: :exception
 
   private
 
@@ -32,5 +28,19 @@ class ApplicationController < ActionController::Base
       expires: 10.days.from_now
     }
     cookies[:cart]
+  end
+
+   # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    puts "----------- #{@current_user.first_name}"
+  end
+  helper_method :current_user
+
+  def authorize
+    redirect_to '/login' unless current_user
   end
 end
